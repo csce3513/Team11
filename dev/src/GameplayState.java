0f453a519207eb0616a171f97fda7fa52bace96c
mdang2711 <mdang@uark.edu>
2012-02-15 16:01:38 -0600

    Connect between Main Menu Screen to the 1st Room

--- dev/src/GameplayState.java
+++ dev/src/GameplayState.java
@@ -1,7 +1,10 @@
 
 
+import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
@@ -9,7 +12,13 @@ import org.newdawn.slick.state.StateBasedGame;
 public class GameplayState extends BasicGameState {
  
     int stateID = 1;
- 
+    Image background = null;
+   	// player
+   	Image player = null;
+   	float x = 400;
+   	float y = 300;
+   	float scale = 1.0f;
+   	
     GameplayState( int stateID ) 
     {
        this.stateID = stateID;
@@ -19,17 +28,60 @@ public class GameplayState extends BasicGameState {
     public int getID() {
         return stateID;
     }
- 
-    public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
- 
+   
+	
+    public GameplayState()
+    {
+        
     }
  
-    public void render(GameContainer gc, StateBasedGame sbg, Graphics gc1) throws SlickException {
- 
+    public void init(GameContainer arg0, StateBasedGame arg1) throws SlickException 
+	{
+    	background = new Image("data/backgroundtiled.png");
+    	player = new Image("data/player.png");
     }
  
-    public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
+    public void update(GameContainer gc, StateBasedGame sb, int delta) throws SlickException     
+    {
+    	Input input = gc.getInput();
+    	 
+        if(input.isKeyDown(Input.KEY_A))
+        {
+            player.rotate(-0.2f * delta);
+        }
  
+        if(input.isKeyDown(Input.KEY_D))
+        {
+            player.rotate(0.2f * delta);
+        }
+ 
+        if(input.isKeyDown(Input.KEY_W))
+        {
+            float hip = 0.4f * delta;
+ 
+            float rotation = player.getRotation();
+ 
+            x+= hip * Math.sin(Math.toRadians(rotation));
+            y-= hip * Math.cos(Math.toRadians(rotation));
+        }
+ 
+        if(input.isKeyDown(Input.KEY_2))
+        {
+            scale += (scale >= 5.0f) ? 0 : 0.1f;
+            player.setCenterOfRotation(player.getWidth()/2.0f*scale, player.getHeight()/2.0f*scale);
+        }
+        if(input.isKeyDown(Input.KEY_1))
+        {
+            scale -= (scale <= 1.0f) ? 0 : 0.1f;
+            player.setCenterOfRotation(player.getWidth()/2.0f*scale, player.getHeight()/2.0f*scale);
+        }
+    }
+ 
+    public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2) throws SlickException 
+    {
+    	background.draw(0,0);
+    	player.draw(x,y,scale);
     }
  
 }
+    
