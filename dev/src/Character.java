e322f4f11a9ca5bbcd855b35172c91c3f673ce22
Karl Northfell <knorthfe@uark.edu>
2012-02-17 14:43:42 -0600

    Get/set in character.java

--- dev/src/Character.java
+++ dev/src/Character.java
@@ -1,4 +1,51 @@
+import org.newdawn.slick.Image;
 
-public class Character {
 
+public abstract class Character 
+{
+	private Image img = null;
+	private double x = 400;
+	private double y = 400;
+	private double scale = 0;
+	private double speed = 0.5;
+	
+	public Character(double xPos, double yPos, double Scale, double Speed)
+	{
+		x = xPos;
+		y = yPos;
+		scale = Scale;
+		speed = Speed;
+	}
+	public void setX(double X)
+	{
+		x = X;
+	}
+	public double getX()
+	{
+		return x;
+	}
+	public void setY(double Y)
+	{
+		y = Y;
+	}
+	public double getY()
+	{
+		return y;
+	}
+	public void setScale(double Scale)
+	{
+		scale = Scale;
+	}
+	public double getScale()
+	{
+		return scale;
+	}
+	public void setSpeed(double Speed)
+	{
+		speed = Speed;
+	}
+	public double getSpeed()
+	{
+		return speed;
+	}
 }
