dbc35df2acd7c859abee9daf8f08b5436464dd63
Thomas Bartlett <tjbartle@uark.edu>
2012-02-25 14:34:35 -0600

    test folder added

---
+++ dev/test/MyClassTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class MyClassTest {
+
+	@Test
+	public void testMultiply() {
+		MyClass tester = new MyClass();
+		assertEquals("Result", 50, tester.multiply(10, 5));
+	}
+
+}
