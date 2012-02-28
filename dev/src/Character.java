ed1b750c29dc452ef5bc1dd63d7d5502091e7c1f
Karl Northfell <knorthfe@uark.edu>
2012-02-28 10:40:09 -0600

    testing and src

--- dev/src/Character.java
+++ dev/src/Character.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
@@ -15,11 +16,26 @@ public class Character
 	private float speed = 0.4f;
 	private String location = "data/player.png";
 	public Character(float xPos, float yPos, float Scale, float Speed) throws SlickException
+=======
+import org.newdawn.slick.Image;
+
+
+public abstract class Character 
+{
+	private Image img = null;
+	private double x = 400;
+	private double y = 400;
+	private double scale = 0;
+	private double speed = 0.5;
+	
+	public Character(double xPos, double yPos, double Scale, double Speed)
+>>>>>>> 2628ac4420c9d6108dc53f56c04e072844734ad8
 	{
 		x = xPos;
 		y = yPos;
 		scale = Scale;
 		speed = Speed;
+<<<<<<< HEAD
 		img = new Image("data/character.png");
 	}
 	public void setX(float X)
@@ -62,4 +78,39 @@ public class Character
 	{
 		return img;
 	}
+=======
+	}
+	public void setX(double X)
+	{
+		x = X;
+	}
+	public double getX()
+	{
+		return x;
+	}
+	public void setY(double Y)
+	{
+		y = Y;
+	}
+	public double getY()
+	{
+		return y;
+	}
+	public void setScale(double Scale)
+	{
+		scale = Scale;
+	}
+	public double getScale()
+	{
+		return scale;
+	}
+	public void setSpeed(double Speed)
+	{
+		speed = Speed;
+	}
+	public double getSpeed()
+	{
+		return speed;
+	}
+>>>>>>> 2628ac4420c9d6108dc53f56c04e072844734ad8
 }
